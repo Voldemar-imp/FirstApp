e43ac7d5a57bf88b82b9eb51e56c4a2ad590db3a
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-06-22 19:56:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteGame = "Battle Brothers";
+            byte myAge = 34;
+            float myHeight = 1.76f;
+            int heightElbrus = 5642;
+            sbyte apartmentNumber = 7;
+            double аbsoluteZero = -273.15;
+            bool earthIsRound = true;
+            short jupitersМoons = 80;
+            ulong citizensVolgograd = 1001183;
+            long citizensRzhev = 56399;
+
+        }
+    }
+}
